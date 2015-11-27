1dcab4b503057db806ca2c81dd96dd457ffc687a
zhenhui <shizhh126@126.com>
2015-11-27 11:21:32 +0800

    create

---
+++ src/test/srcTest.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+import java.util.*;
+import std.StdIn;
+import std.StdOut;
+
+/**
+ *
+ * @author zhenhui
+ */
+public class srcTest {
+    public static void main(String[] args){
+        int[] a = {3,4,1,1,2};
+        Arrays.sort(a);
+    }
+}
